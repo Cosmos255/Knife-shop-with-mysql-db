b352e700764bb0663bef2fd944c455b698e7c327
Cosmo <gorcadaniel252@gmail.com>
2024-11-16 15:57:24 +0200

    changes

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let productsGrid = document.getElementById("products-grid");
 let productsArray = [];
 
+
+//I like fetch with async/await over xhr
 async function fetchDatabase(){
-    let url="";
+
+    let url="https://my-json-server.typicode.com/Cosmos255/Ceva-cu-jquery/products";
+
     try{
+        
         const response = await fetch(url);
 
         if(!response.ok){
@@ -11,8 +16,27 @@ async function fetchDatabase(){
         }
 
         const data = await response.json()
+        console.log(data);
+        productsGrid.innerHTML = null;
+        data.forEach(p =>{
+            productsArray.push(p);
+            let pElem = document.createElement('div');
+            pElem.classList.add('product');
+            pElem.innerHTML = `
+                                <h2 class="product-name">${p.name}</h2>
+                                <img src="${p.photo_url}"class="products-img">
+                                <p class="product-price><b>Price: </b>${p.price}</p>
+                                <p class="product-description"><b>Description: </b>${p.description}</p>`;
+                    productsGrid.append(pElem);
+        });
+
+
+
     }
     catch(error){
         console.error(error);
     }
 }
+
+fetchDatabase();
+

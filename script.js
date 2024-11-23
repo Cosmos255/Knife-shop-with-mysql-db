4febbcb4a6a6fa36bbebcb17b4cc1f2d2cda0768
Cosmo <gorcadaniel252@gmail.com>
2024-11-23 13:56:51 +0200

    update

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ async function fetchDatabase(){
             pElem.classList.add('product');
             pElem.innerHTML = `
                                 <h2 class="product-name">${p.name}</h2>
-                                <img src="${p.photo_url}"class="products-img">
-                                <p class="product-price><b>Price: </b>${p.price}</p>
+                                <img src="${p.photo_url}"class="products-img"></img>
+                                <p class="product-price"><b>Price: </b>${p.price}</p>
                                 <p class="product-description"><b>Description: </b>${p.description}</p>`;
                     productsGrid.append(pElem);
         });
